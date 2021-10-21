8dc28446a732699970cc148a4ba2f82af4df3fc0
Nikolai Evans <75125482+Nikev2@users.noreply.github.com>
2021-10-20 19:57:02 -0700

    Update Core.lua

--- NavySimulator/Core.lua
+++ NavySimulator/Core.lua
@@ -255,7 +255,7 @@ RaidA52.MouseButton1Click:Connect(function()
 	local Area52 = game:GetService("Workspace").Islands["Area 52"].EssentialWorkers
 	for i,v in pairs(Area52:GetDescendants()) do
 			if v.Name == "Prompt" then
-				fireproximityprompt(v, 10)
+				fireproximityprompt(v, 20)
 				print(v.Name)
 				print("C4Planted")
 				 
